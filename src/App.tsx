b88dbd710bb6481aa3e3e917081b56d05739c4a3
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-06-25 02:36:44 -0300

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-// import Home from "./pages/Home";
+
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
 import NewRoom from "./pages/NewRoom";
 
 function App() {
 	return (
-		<div className="App">
-			<NewRoom />
-		</div>
+		<BrowserRouter>
+			<Route path="/" exact component={Home} />
+			<Route path="/rooms/new" component={NewRoom} />
+		</BrowserRouter>
 	);
 }
 
